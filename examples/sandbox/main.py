ce7a740a0813c0a34ebe7bef6a285c52cca7bc63
madvn <madvncv@gmail.com>
2019-12-30 17:16:46 -0500

    2to3

--- examples/sandbox/main.py
+++ examples/sandbox/main.py
@@ -2,11 +2,13 @@ import tensorflow as tf
 import numpy as np
 from constTestClass import *
 
+
 def const_test_main():
     sess = tf.Session()
-    const_test = ConstTest(np.ones(5)*1.)
+    const_test = ConstTest(np.ones(5) * 1.0)
     for i in range(5):
-        print sess.run(const_test.output_T,feed_dict={input_pl:np.ones(5)*i})
+        print(sess.run(const_test.output_T, feed_dict={input_pl: np.ones(5) * i}))
+
 
 if __name__ == "__main__":
     const_test_main()
